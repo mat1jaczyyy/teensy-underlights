5c917ba194a48139c598b6e9e9991e50474aea9e
mat1jaczyyy <dominik.matijaca@skole.hr>
2018-04-24 22:14:25 +0200

    Retina Converter - specify stripMax

--- retinaConverter.py
+++ retinaConverter.py
@@ -1,31 +1,18 @@
 x = input().split(";")
 
-print("#if STRIP == 0")
+#stripMax = 255  # WS2812B
+#stripMax = 127  # LPD8806
 
-mul = float(255) / float(63)
+mul = float(stripMax) / float(63)
 
-r = "  const uint8_t _r[128] = {"
-g = "  const uint8_t _g[128] = {"
-b = "  const uint8_t _b[128] = {"
+r = "const byte _r[128] = {"
+g = "const byte _g[128] = {"
+b = "const byte _b[128] = {"
 
 for _ in range(128):
 	a = x[_].split()	
 	r += str(round(float(a[1]) * mul)) + (", " if _ < 127 else "};\n")
 	g += str(round(float(a[2]) * mul)) + (", " if _ < 127 else "};\n")
-	b += str(round(float(a[3]) * mul)) + (", " if _ < 127 else "};\n")
+	b += str(round(float(a[3]) * mul)) + (", " if _ < 127 else "};")
 
-print(r + g + b + "#elif STRIP == 1")
-
-mul = float(127) / float(63)
-
-r = "  const uint8_t _r[128] = {"
-g = "  const uint8_t _g[128] = {"
-b = "  const uint8_t _b[128] = {"
-
-for _ in range(128):
-	a = x[_].split()
-	r += str(round(float(a[1]) * mul)) + (", " if _ < 127 else "};\n")
-	g += str(round(float(a[2]) * mul)) + (", " if _ < 127 else "};\n")
-	b += str(round(float(a[3]) * mul)) + (", " if _ < 127 else "};\n")
-
-print(r + g + b + "#endif")
+print(r + g + b)
