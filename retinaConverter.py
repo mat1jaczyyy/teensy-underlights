42d2c1ad6e7522f5f10fb225e3f1d38449afc36c
mat1jaczyyy <dominik.matijaca@skole.hr>
2018-06-08 23:00:55 +0200

    Retina Converter: fix issue with LF character

--- retinaConverter.py
+++ retinaConverter.py
@@ -1,5 +1,11 @@
-x = input().split(";")
-
+x = []
+z = ""
+try:
+	while True:
+		z += input()
+except EOFError:
+	x = z.split(";")
+	
 #stripMax = 255  # WS2812B
 #stripMax = 127  # LPD8806
 
@@ -10,7 +16,7 @@ g = "const byte _g[128] = {"
 b = "const byte _b[128] = {"
 
 for _ in range(128):
-	a = x[_].split()	
+	a = x[_].split()
 	r += str(round(float(a[1]) * mul)) + (", " if _ < 127 else "};\n")
 	g += str(round(float(a[2]) * mul)) + (", " if _ < 127 else "};\n")
 	b += str(round(float(a[3]) * mul)) + (", " if _ < 127 else "};")
